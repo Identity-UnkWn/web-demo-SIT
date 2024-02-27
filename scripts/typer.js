726a92fa403337563fc50f8598e639b986c84beb
Identity-UnkWn <147016635+Identity-UnkWn@users.noreply.github.com>
2024-02-27 15:28:19 +0530

    Add files via upload

---
+++ scripts/typer.js
@@ -0,0 +1,8 @@
+var typerr = new Typed(".typer",
+    {
+        strings: ["Siddaganga", "Institute", "Of", "Technology"],
+        typeSpeed: 100,
+        backSpeed: 15,
+        loop: true
+    }
+);
